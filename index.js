d70c9b5aa58f24d5f408c6bfc455b99eb45f2d9d
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-04-29 22:35:23 +0200

    Projet fini

---
+++ index.js
@@ -0,0 +1,33 @@
+// @ts-nocheck
+const counterDisplay = document.querySelector("h3");
+let count = 0;
+
+const bubbleMaker = () => {
+  const bubble = document.createElement("span");
+  bubble.classList.add("bubble");
+  document.body.appendChild(bubble);
+
+  const size = `${Math.random() * 200 + 100}px`;
+  bubble.style.height = size;
+  bubble.style.width = size;
+
+  const topPosition = `${Math.random() * 100 + 50}%`;
+  const leftPosition = `${Math.random() * 100}%`;
+  bubble.style.top = topPosition;
+  bubble.style.left = leftPosition;
+
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+  bubble.style.setProperty("--left", `${Math.random() * 100 * plusMinus}%`);
+
+  bubble.addEventListener("click", () => {
+    count++;
+    counterDisplay.textContent = count;
+    bubble.remove();
+  });
+
+  setTimeout(() => {
+    bubble.remove();
+  }, 10000);
+};
+
+setInterval(bubbleMaker, 100);
